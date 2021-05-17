73620ced912f98d149337272cba5c4aef9bfb3ea
Álvaro <51362780+SkulleD@users.noreply.github.com>
2021-05-17 20:37:44 +0200

    Add files via upload

--- 3EVA_DI_ej4/Form1.Designer.cs
+++ 3EVA_DI_ej4/Form1.Designer.cs
@@ -44,26 +44,26 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(11, 58);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Location = new System.Drawing.Point(15, 71);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(76, 20);
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
             this.textBox1.TabIndex = 1;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(134, 58);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox2.Location = new System.Drawing.Point(179, 71);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(76, 20);
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
             this.textBox2.TabIndex = 2;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(82, 28);
-            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Location = new System.Drawing.Point(109, 34);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(56, 25);
+            this.button1.Size = new System.Drawing.Size(75, 31);
             this.button1.TabIndex = 0;
             this.button1.Text = "Calcular";
             this.button1.UseVisualStyleBackColor = true;
@@ -73,83 +73,76 @@
             // 
             this.radioButton1.AutoSize = true;
             this.radioButton1.Checked = true;
-            this.radioButton1.Location = new System.Drawing.Point(9, 95);
-            this.radioButton1.Margin = new System.Windows.Forms.Padding(2);
+            this.radioButton1.Location = new System.Drawing.Point(12, 117);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(55, 17);
+            this.radioButton1.Size = new System.Drawing.Size(70, 21);
             this.radioButton1.TabIndex = 3;
             this.radioButton1.TabStop = true;
             this.radioButton1.Text = "Sumar";
             this.radioButton1.UseVisualStyleBackColor = true;
-            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
             this.radioButton1.Click += new System.EventHandler(this.radioButtons);
             // 
             // radioButton2
             // 
             this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(9, 129);
-            this.radioButton2.Margin = new System.Windows.Forms.Padding(2);
+            this.radioButton2.Location = new System.Drawing.Point(12, 159);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(56, 17);
+            this.radioButton2.Size = new System.Drawing.Size(71, 21);
             this.radioButton2.TabIndex = 5;
             this.radioButton2.Text = "Restar";
             this.radioButton2.UseVisualStyleBackColor = true;
-            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
             this.radioButton2.Click += new System.EventHandler(this.radioButtons);
             // 
             // radioButton3
             // 
             this.radioButton3.AutoSize = true;
-            this.radioButton3.Location = new System.Drawing.Point(127, 95);
-            this.radioButton3.Margin = new System.Windows.Forms.Padding(2);
+            this.radioButton3.Location = new System.Drawing.Point(169, 117);
+            this.radioButton3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(72, 17);
+            this.radioButton3.Size = new System.Drawing.Size(92, 21);
             this.radioButton3.TabIndex = 4;
             this.radioButton3.Text = "Multiplicar";
             this.radioButton3.UseVisualStyleBackColor = true;
-            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
             this.radioButton3.Click += new System.EventHandler(this.radioButtons);
             // 
             // radioButton4
             // 
             this.radioButton4.AutoSize = true;
-            this.radioButton4.Location = new System.Drawing.Point(127, 129);
-            this.radioButton4.Margin = new System.Windows.Forms.Padding(2);
+            this.radioButton4.Location = new System.Drawing.Point(169, 159);
+            this.radioButton4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.radioButton4.Name = "radioButton4";
-            this.radioButton4.Size = new System.Drawing.Size(54, 17);
+            this.radioButton4.Size = new System.Drawing.Size(68, 21);
             this.radioButton4.TabIndex = 6;
             this.radioButton4.Text = "Dividir";
             this.radioButton4.UseVisualStyleBackColor = true;
-            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
             this.radioButton4.Click += new System.EventHandler(this.radioButtons);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(105, 60);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(140, 74);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.Size = new System.Drawing.Size(16, 17);
             this.label1.TabIndex = 7;
             this.label1.Text = "+";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(223, 60);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(297, 74);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.Size = new System.Drawing.Size(16, 17);
             this.label2.TabIndex = 8;
             this.label2.Text = "=";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(246, 60);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(328, 74);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.Size = new System.Drawing.Size(0, 17);
             this.label3.TabIndex = 9;
             // 
             // timer1
@@ -158,9 +151,9 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -171,7 +164,7 @@
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
--- 3EVA_DI_ej4/Form1.cs
+++ 3EVA_DI_ej4/Form1.cs
@@ -14,7 +14,11 @@ namespace _3EVA_DI_ej4
     public partial class Form1 : Form
     {
         public delegate int delCalcular(int n1, int n2);
-        public delCalcular funcion;
+        public delCalcular fSuma;
+        public delCalcular fResta;
+        public delCalcular fMulti;
+        public delCalcular fDivision;
+        public delCalcular fFinal;
         public Hashtable tabla;
         int resultado;
         int minutos = 0, segundos = 0;
@@ -27,17 +31,22 @@ namespace _3EVA_DI_ej4
             timer1.Tick += new EventHandler(timer1_Tick);
             timer1.Interval = 1000;
             timer1.Start();
-            funcion = Suma;
+            label1.Text = "+";
+
             tabla = new Hashtable();
-            tabla.Add(radioButton1.Text, (delCalcular)Suma);
-            tabla.Add(radioButton2.Text, (delCalcular)Resta);
-            tabla.Add(radioButton3.Text, (delCalcular)Multiplicacion);
-            tabla.Add(radioButton4.Text, (delCalcular)Division);
+            tabla.Add(radioButton1.Text, (delCalcular)fSuma);
+            tabla.Add(radioButton2.Text, (delCalcular)fResta);
+            tabla.Add(radioButton3.Text, (delCalcular)fMulti);
+            tabla.Add(radioButton4.Text, (delCalcular)fDivision);
 
             radioButton1.Tag = "+";
             radioButton2.Tag = "-";
             radioButton3.Tag = "*";
             radioButton4.Tag = "/";
+            fSuma = (n1, n2) => n1 + n2;
+            fResta = (n1, n2) => n1 - n2;
+            fMulti = (n1, n2) => n1 * n2;
+            fDivision = (n1, n2) => n1 / n2;
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -49,38 +58,42 @@ namespace _3EVA_DI_ej4
         {
             try
             {
-                resultado = funcion(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text));
+                resultado = fFinal(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text));
                 label3.Text = resultado.ToString();
             }
             catch (FormatException)
             {
 
             }
-        }
+            catch (NullReferenceException) // No la captura ????
+            {
 
-        private void radioButton1_CheckedChanged(object sender, EventArgs e)
-        {
-            funcion = (n1, n2) => n1 + n2;
-        }
-
-        private void radioButton2_CheckedChanged(object sender, EventArgs e)
-        {
-            funcion = (n1, n2) => n1 - n2;
-        }
-
-        private void radioButton3_CheckedChanged(object sender, EventArgs e)
-        {
-            funcion = (n1, n2) => n1 * n2;
-        }
-
-        private void radioButton4_CheckedChanged(object sender, EventArgs e)
-        {
-            funcion = (n1, n2) => n1 / n2;
+            }
         }
 
         private void radioButtons(object sender, EventArgs e)
         {
             label1.Text = (string)((RadioButton)sender).Tag;
+            
+            if (tabla.ContainsKey("+"))
+            {
+                fFinal = fSuma;
+            }
+
+            if (tabla.ContainsKey("-"))
+            {
+                fFinal = fResta;
+            }
+
+            if (tabla.ContainsKey("*"))
+            {
+                fFinal = fMulti;
+            }
+
+            if (tabla.ContainsKey("/"))
+            {
+                fFinal = fDivision;
+            }
         }
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
@@ -96,31 +109,6 @@ namespace _3EVA_DI_ej4
                 e.Cancel = true;
             }
         }
-
-        private int Suma(int n1, int n2)
-        {
-
-            return n1 + n2;
-        }
-
-        private int Resta(int n1, int n2)
-        {
-
-            return n1 - n2;
-        }
-
-        private int Multiplicacion(int n1, int n2)
-        {
-
-            return n1 * n2;
-        }
-
-        private int Division(int n1, int n2)
-        {
-
-            return n1 / n2;
-        }
-
 
         private void timer1_Tick(object sender, EventArgs e)
         {

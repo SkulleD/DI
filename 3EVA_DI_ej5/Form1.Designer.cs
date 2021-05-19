bb348047090aae80abe14c6ca777d392b96f96e2
Álvaro <51362780+SkulleD@users.noreply.github.com>
2021-05-19 14:01:49 +0200

    Add files via upload

--- 3EVA_DI_ej5/Form1.Designer.cs
+++ 3EVA_DI_ej5/Form1.Designer.cs
@@ -42,89 +42,101 @@
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(64, 61);
+            this.listBox1.Location = new System.Drawing.Point(124, 20);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(2);
             this.listBox1.Name = "listBox1";
             this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
-            this.listBox1.Size = new System.Drawing.Size(120, 84);
-            this.listBox1.TabIndex = 0;
+            this.listBox1.Size = new System.Drawing.Size(91, 147);
+            this.listBox1.TabIndex = 2;
             // 
             // listBox2
             // 
             this.listBox2.FormattingEnabled = true;
-            this.listBox2.ItemHeight = 16;
-            this.listBox2.Location = new System.Drawing.Point(278, 61);
+            this.listBox2.Location = new System.Drawing.Point(334, 20);
+            this.listBox2.Margin = new System.Windows.Forms.Padding(2);
             this.listBox2.Name = "listBox2";
             this.listBox2.SelectionMode = System.Windows.Forms.SelectionMode.None;
-            this.listBox2.Size = new System.Drawing.Size(120, 84);
-            this.listBox2.TabIndex = 1;
+            this.listBox2.Size = new System.Drawing.Size(91, 147);
+            this.listBox2.TabIndex = 5;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(27, 163);
+            this.button1.Location = new System.Drawing.Point(32, 54);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 2;
+            this.button1.Size = new System.Drawing.Size(57, 23);
+            this.button1.TabIndex = 1;
             this.button1.Text = "AÑADIR";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(135, 163);
+            this.button2.Location = new System.Drawing.Point(33, 81);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 3;
+            this.button2.Size = new System.Drawing.Size(56, 23);
+            this.button2.TabIndex = 4;
+            this.button2.TabStop = false;
             this.button2.Text = "QUITAR";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(240, 163);
+            this.button3.Location = new System.Drawing.Point(219, 20);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(99, 23);
-            this.button3.TabIndex = 4;
-            this.button3.Text = "TRASPASAR";
+            this.button3.Size = new System.Drawing.Size(111, 19);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "TRASPASAR-->";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(345, 163);
+            this.button4.Location = new System.Drawing.Point(219, 148);
+            this.button4.Margin = new System.Windows.Forms.Padding(2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(101, 23);
-            this.button4.TabIndex = 5;
-            this.button4.Text = "TRASPASAR";
+            this.button4.Size = new System.Drawing.Size(111, 19);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "<--TRASPASAR";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(449, 77);
+            this.textBox1.Location = new System.Drawing.Point(19, 30);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 22);
-            this.textBox1.TabIndex = 6;
+            this.textBox1.Size = new System.Drawing.Size(87, 20);
+            this.textBox1.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(488, 149);
+            this.label1.Location = new System.Drawing.Point(235, 64);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.Size = new System.Drawing.Size(82, 13);
             this.label1.TabIndex = 7;
-            this.label1.Text = "label1";
+            this.label1.Text = "Nº elementos: 0";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(488, 194);
+            this.label2.Location = new System.Drawing.Point(252, 91);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.Size = new System.Drawing.Size(39, 13);
             this.label2.TabIndex = 8;
-            this.label2.Text = "label2";
+            this.label2.Text = "Índice:";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
@@ -134,6 +146,7 @@
             this.Controls.Add(this.button1);
             this.Controls.Add(this.listBox2);
             this.Controls.Add(this.listBox1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
--- 3EVA_DI_ej5/Form1.cs
+++ 3EVA_DI_ej5/Form1.cs
@@ -12,14 +12,52 @@ namespace _3EVA_DI_ej5
 {
     public partial class Form1 : Form
     {
+        ToolTip tip;
         public Form1()
         {
             InitializeComponent();
+            tip = new ToolTip();
+            this.AcceptButton = button1;
+            tip.SetToolTip(this.button1, "Añade un nuevo elemento a la lista 1");
+            tip.SetToolTip(this.button2, "Elimina un elemento de la lista 1");
+            tip.SetToolTip(this.button3, "Mueve todos los elementos de la lista 1 a la lista 2");
+            tip.SetToolTip(this.button4, "Mueve todos los elementos de la lista 2 a la lista 1");
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                listBox1.Items.Add(textBox1.Text);
+                textBox1.Text = "";
+                label1.Text = "Nº elementos: " + listBox1.Items.Count.ToString();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Remove(listBox1.SelectedItem);
+            label2.Text = "Índice: " + listBox1.SelectedIndex.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                listBox2.Items.Insert(i, listBox1.Items.ToString());
+            }
+            listBox1.Items.Clear();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.AddRange(listBox2.Items);
+            listBox2.Items.Clear();
         }
     }
 }

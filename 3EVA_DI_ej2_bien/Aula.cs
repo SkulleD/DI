e419110ec4e4f27cca1cb79e2f0aa0b69bd41446
Álvaro <51362780+SkulleD@users.noreply.github.com>
2021-05-23 22:54:59 +0200

    Add files via upload

---
+++ 3EVA_DI_ej2_bien/Aula.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _3EVA_DI_ej2_bien
+{
+    class Aula
+    {
+        //public delegate int delegado(int a);
+        //public delegado tabla;
+        Random random = new Random();
+        int rand;
+        int porcentaje;
+        int acu = 0;
+        public int[,] notas = new int[12, 4];
+        public string[] alumnos = new string[12];
+        public enum Asignaturas
+        {
+            Matemáticas,
+            Biología,
+            Lengua,
+            Inglés
+        }
+
+        public void CrearAlumnos()
+        {
+            for (int i = 0; i < 12; i++)
+            {
+                alumnos[i] = "Alumno " + i;
+            }
+
+            //foreach (string elemento in alumnos)
+            //{
+            //    Console.WriteLine(elemento);
+            //}
+        }
+
+        public int RellenaTabla()
+        {
+            acu = 0;
+            for (int i = 0; i < 12; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    porcentaje = random.Next(1, 101);
+
+                    if (porcentaje <= 15) // De 0% a 15%. 5% cada nota
+                    {
+                        rand = random.Next(0, 3); // 0, 1, 2
+                        notas[i, j] = rand;
+                    }
+
+                    if (porcentaje > 15 && porcentaje <= 25) // De 16% a 25% 
+                    {
+                        rand = 3; // 3
+                        notas[i, j] = rand;
+                    }
+
+                    if (porcentaje > 25 && porcentaje <= 70) // De 26% a 70%. 15% cada nota
+                    {
+                        rand = random.Next(4, 7); // 4, 5, 6
+                        notas[i, j] = rand;
+                    }
+
+                    if (porcentaje > 70 && porcentaje <= 90) // De 71% a 90%. 10% cada nota
+                    {
+                        rand = random.Next(7, 9); // 7, 9
+                        notas[i, j] = rand;
+                    }
+
+                    if (porcentaje > 90 && porcentaje <= 100) // De 91% a 100%. 5% cada nota
+                    {
+                        rand = random.Next(9, 11); // 9, 10
+                        notas[i, j] = rand;
+                    }
+                    acu = acu + rand;
+                }
+            }
+            return acu;
+        }
+
+        public int CalculaMedia()
+        {
+
+            int acu = 0;
+
+            for (int i = 0; i < notas.GetLength(0); i++)
+            {
+                acu = acu + notas[i, 0];
+            }
+            return acu;
+        }
+    }
+}
---
+++ 3EVA_DI_ej2_bien/Interfaz.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _3EVA_DI_ej2_bien
+{
+    class Interfaz
+    {
+        Aula aula = new Aula();
+        bool bandera;
+
+        public void menuPrincipal()
+        {
+            int menu;
+            aula.RellenaTabla();
+
+            do
+            {
+                Console.WriteLine("--- Elige una opción ---\n" +
+                "1- Calcular media de notas de la tabla\n" +
+                "2- Media de un alumno\n" +
+                "3- Media de una asignatura\n" +
+                "4- Visualizar notas de un alumno\n" +
+                "5- Visualizar notas de una asignatura\n" +
+                "6- Nota máxima y mínima de un alumno\n" +
+                "7- Tabla de aprobados\n" +
+                "8- Visualizar tabla completa\n" +
+                "9- Salir del programa");
+
+                menu = PideInt();
+                switch (menu)
+                {
+                    case 1:
+                        Console.WriteLine(aula.CalculaMedia());
+                        break;
+                    case 2:
+                        Console.Write("¿Nº del alumno?");
+
+                        break;
+                    case 3:
+                        Console.Write("¿Nº de la asignatura?");
+
+                        break;
+                    case 4:
+                        Console.Write("¿Nº del alumno?");
+
+                        break;
+                    case 5:
+                        Console.Write("¿Nº de la asignatura?");
+
+                        break;
+                    case 6:
+                        Console.Write("¿Nº del alumno?");
+
+                        break;
+                    case 7:
+
+                        break;
+                    case 8:
+
+                        break;
+                    case 9:
+                        Console.WriteLine("Saliendo del programa...");
+                        break;
+                    default:
+
+                        break;
+                }
+            } while (menu != 9);
+        }
+
+        public int PideInt()
+        {
+            int num = 0;
+            do
+            {
+                bandera = false;
+                try
+                {
+                    num = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (FormatException)
+                {
+                    bandera = true;
+                    Console.Write("\nError de formato. Solo se pueden introducir números");
+                }
+                catch (OverflowException)
+                {
+                    bandera = true;
+                    Console.Write("\nError de desbordamiento");
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    bandera = true;
+                    Console.Write("\nRecuerda que hay no hay más de 12 alumnos ni más de 4 asignaturas.");
+                }
+            } while (bandera);
+            return num;
+        }
+    }
+}

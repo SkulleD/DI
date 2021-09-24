1fdd4a37a3b8d7a9f98e3b4b2c09d986e4072067
Álvaro <51362780+SkulleD@users.noreply.github.com>
2021-09-24 13:50:30 +0200

    Add files via upload

---
+++ Boletin2/Boletin2/Empleado.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boletin2
+{
+    class Empleado : Persona
+    {
+        public double salario;
+        private double irpf;
+        public string telefono;
+
+        public double Salario
+        {
+            set
+            {
+
+            }
+            get
+            {
+
+            }
+        }
+
+        public double IRPF { set; get; }
+
+        public string Telefono
+        {
+            set { }
+            get
+            {
+                return "+34 " + telefono;
+            }
+        }
+
+        public override void MuestraCampos()
+        {
+            base.MuestraCampos();
+            Console.WriteLine(salario);
+            Console.WriteLine(telefono);
+        }
+
+        public override void IntroCampos(string nombre, string apellidos, int edad, string dni)
+        {
+            
+        }
+
+        public Empleado(double salario, string telefono) : base("", "", 0, "")
+        {
+            this.Salario = salario;
+            this.Telefono = telefono;
+        }
+
+        public Empleado() : this(0, "") {
+
+        }
+
+        public override double Hacienda()
+        {
+            return IRPF * Salario / 100;
+        }
+    }
+}
---
+++ Boletin2/Boletin2/Persona.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boletin2
+{
+    abstract class Persona
+    {
+        private string nombre;
+        private string apellidos;
+        private int edad;
+        private string dni;
+
+        public string Nombre { set; get; }
+        public string Apellidos { set; get; }
+
+        public int Edad
+        {
+            set
+            {
+                if (value < 0)
+                {
+                    edad = 0;
+                }
+                else
+                {
+                    edad = value;
+                }
+            }
+            get
+            {
+                return edad;
+            }
+        }
+
+        public string DNI
+        {
+            set
+            {
+                dni = value;
+            }
+            get
+            {
+                return dni;
+            }
+        }
+
+        public Persona(string nombre, string apellidos, int edad, string dni)
+        {
+            this.Nombre = nombre;
+            this.Apellidos = apellidos;
+            this.Edad = edad;
+            this.DNI = dni;
+        }
+
+        public Persona() : this("", "", 0, "")
+        {
+
+        }
+
+        public virtual void MuestraCampos() {
+            Console.WriteLine(nombre);
+            Console.WriteLine(apellidos);
+            Console.WriteLine(edad);
+            Console.WriteLine(dni);
+        }
+
+        public virtual void IntroCampos(string nombre, string apellidos, int edad, string dni)
+        {
+            nombre = Console.ReadLine();
+            apellidos = Console.ReadLine();
+            edad = Convert.ToInt32(Console.ReadLine());
+            dni = Console.ReadLine();
+        }
+
+        public abstract double Hacienda();
+    }
+}

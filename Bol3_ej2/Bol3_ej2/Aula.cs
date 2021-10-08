ce49f0edd6bab92291c5903d8612fc042e545fdd
Álvaro <51362780+SkulleD@users.noreply.github.com>
2021-10-08 13:52:24 +0200

    Add files via upload

--- Bol3_ej2/Bol3_ej2/Aula.cs
+++ Bol3_ej2/Bol3_ej2/Aula.cs
@@ -8,6 +8,7 @@ namespace Bol3_ej2
 {
     class Aula
     {
+        Random random = new Random();
         private string[] alumnos = { "Alumno1", "Alumno2", "Alumno3", "Alumno4", "Alumno5" +
                 "Alumno6", "Alumno7", "Alumno8", "Alumno9", "Alumno10" };
         public string[] Alumnos
@@ -30,6 +31,23 @@ namespace Bol3_ej2
         public Aula(string[] nombres)
         {
             notas = new int[4, alumnos.Length];
+            RellenaNotas();
+        }
+
+        public void RellenaNotas()
+        {
+            for (int i = 0; i < notas[i, 0]; i++)
+            {
+                for (int j = 0; j < notas[0, j]; j++)
+                {
+                    notas[i, j] = random.Next(0, 11);
+                }
+            }
+        }
+
+        public int[,] MuestraNotas()
+        {
+            return notas;
         }
     }
 }
--- Bol3_ej2/Bol3_ej2/Menu.cs
+++ Bol3_ej2/Bol3_ej2/Menu.cs
@@ -11,7 +11,7 @@ namespace Bol3_ej2
         Aula aula;
         private string[] nombres;
 
-        public Menu (params string[] nombres)
+        public Menu(params string[] nombres)
         {
             this.nombres = nombres;
             aula = new Aula(this.nombres);
@@ -21,9 +21,64 @@ namespace Bol3_ej2
         {
             int menu = 0;
 
-            switch (menu)
+            do
             {
+                Console.WriteLine("1- Calcular media de toda la tabla\n" +
+                    "2- Media de un alumno\n" +
+                    "3- Media de una asignatura\n" +
+                    "4- Visualizar notas de un alumno\n" +
+                    "5- Visualizar notas de una asignatura\n" +
+                    "6- Nota máxima y mínima de un alumno\n" +
+                    "7- Lista de alumnos con todo aprobado\n" +
+                    "8- Visualizar tabla completa\n" +
+                    "9- Salir del programa");
 
+                menu = int.Parse(Console.ReadLine());
+                switch (menu)
+                {
+                    case 1:
+
+                        break;
+                    case 2:
+
+                        break;
+                    case 3:
+
+                        break;
+                    case 4:
+
+                        break;
+                    case 5:
+
+                        break;
+                    case 6:
+
+                        break;
+                    case 7:
+
+                        break;
+                    case 8:
+                        MuestraNotas();
+                        break;
+                    case 9:
+                        Console.WriteLine("Nos vemos!");
+                        break;
+                    default:
+                        Console.WriteLine("Solo puede ser un número del 1 al 9");
+                        break;
+                }
+            } while (menu != 9);
+        }
+
+        public void MuestraNotas()
+        {
+            Console.WriteLine("holaa");
+            for (int i = 0; i < aula.notas[i, 0]; i++)
+            {
+                for (int j = 0; j < aula.notas[0, j]; j++)
+                {
+                    Console.WriteLine(aula.notas[i, j]);
+                }
             }
         }
     }

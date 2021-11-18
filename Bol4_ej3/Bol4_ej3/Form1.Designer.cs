06fd0d6f780b97e24a40ff248f9b4ab0182efd49
Álvaro <51362780+SkulleD@users.noreply.github.com>
2021-11-18 12:23:18 +0100

    Add files via upload

---
+++ Bol4_ej3/Bol4_ej3/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿
+namespace Bol4_ej3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnFileDialog = new System.Windows.Forms.Button();
+            this.checkModal = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // btnFileDialog
+            // 
+            this.btnFileDialog.Location = new System.Drawing.Point(92, 91);
+            this.btnFileDialog.Name = "btnFileDialog";
+            this.btnFileDialog.Size = new System.Drawing.Size(75, 23);
+            this.btnFileDialog.TabIndex = 0;
+            this.btnFileDialog.Text = "ABRIR";
+            this.btnFileDialog.UseVisualStyleBackColor = true;
+            this.btnFileDialog.Click += new System.EventHandler(this.btnFileDialog_Click);
+            // 
+            // checkModal
+            // 
+            this.checkModal.AutoSize = true;
+            this.checkModal.Location = new System.Drawing.Point(101, 129);
+            this.checkModal.Name = "checkModal";
+            this.checkModal.Size = new System.Drawing.Size(55, 17);
+            this.checkModal.TabIndex = 1;
+            this.checkModal.Text = "Modal";
+            this.checkModal.UseVisualStyleBackColor = true;
+            this.checkModal.CheckedChanged += new System.EventHandler(this.checkModal_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.checkModal);
+            this.Controls.Add(this.btnFileDialog);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Visor de imágenes";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFileDialog;
+        private System.Windows.Forms.CheckBox checkModal;
+    }
+}
+
---
+++ Bol4_ej3/Bol4_ej3/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bol4_ej3
+{
+    public partial class Form1 : Form
+    {
+        Form2 form2;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnFileDialog_Click(object sender, EventArgs e)
+        {
+            DialogResult res;
+            string fileContent = string.Empty;
+            string filePath = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png|All Files (*.*)|*.*";
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    filePath = openFileDialog.FileName;
+
+                    Stream fileStream = openFileDialog.OpenFile();
+
+                    using (StreamReader reader = new StreamReader(fileStream))
+                    {
+                        fileContent = reader.ReadToEnd();
+                        filePath = Path.GetDirectoryName(fileContent);
+                        form2 = new Form2(fileContent);
+                    }
+                }
+            }
+
+            if (checkModal.Checked)
+            {
+                res = form2.ShowDialog();
+            } else
+            {
+                form2.Show();
+            }
+
+        }
+
+        private void checkModal_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

8a9e9c0906dd528c0bdc0593d8f414fdfbc87a1d
Álvaro <51362780+SkulleD@users.noreply.github.com>
2021-11-24 18:36:30 +0100

    Add files via upload

---
+++ Bol4_ej5/Bol4_ej5/Bol4_ej5/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿
+namespace Bol4_ej5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnMoveRight = new System.Windows.Forms.Button();
+            this.btnMoveLeft = new System.Windows.Forms.Button();
+            this.textIntro = new System.Windows.Forms.TextBox();
+            this.lblinfo = new System.Windows.Forms.Label();
+            this.lblinfoLeft = new System.Windows.Forms.Label();
+            this.lblInfoRight = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(16, 37);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBox1.Size = new System.Drawing.Size(173, 180);
+            this.listBox1.TabIndex = 0;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(307, 37);
+            this.listBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(173, 180);
+            this.listBox2.TabIndex = 4;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(91, 257);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(100, 28);
+            this.btnAdd.TabIndex = 6;
+            this.btnAdd.Text = "ADD";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(199, 64);
+            this.btnRemove.Margin = new System.Windows.Forms.Padding(4);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(100, 28);
+            this.btnRemove.TabIndex = 1;
+            this.btnRemove.Text = "REMOVE";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnMoveRight
+            // 
+            this.btnMoveRight.Location = new System.Drawing.Point(199, 100);
+            this.btnMoveRight.Margin = new System.Windows.Forms.Padding(4);
+            this.btnMoveRight.Name = "btnMoveRight";
+            this.btnMoveRight.Size = new System.Drawing.Size(100, 28);
+            this.btnMoveRight.TabIndex = 2;
+            this.btnMoveRight.Text = "MOVE -->";
+            this.btnMoveRight.UseVisualStyleBackColor = true;
+            this.btnMoveRight.Click += new System.EventHandler(this.btnMoveRight_Click);
+            // 
+            // btnMoveLeft
+            // 
+            this.btnMoveLeft.Location = new System.Drawing.Point(199, 135);
+            this.btnMoveLeft.Margin = new System.Windows.Forms.Padding(4);
+            this.btnMoveLeft.Name = "btnMoveLeft";
+            this.btnMoveLeft.Size = new System.Drawing.Size(100, 28);
+            this.btnMoveLeft.TabIndex = 3;
+            this.btnMoveLeft.Text = "<-- MOVE";
+            this.btnMoveLeft.UseVisualStyleBackColor = true;
+            this.btnMoveLeft.Click += new System.EventHandler(this.btnMoveLeft_Click);
+            // 
+            // textIntro
+            // 
+            this.textIntro.Location = new System.Drawing.Point(16, 225);
+            this.textIntro.Margin = new System.Windows.Forms.Padding(4);
+            this.textIntro.Name = "textIntro";
+            this.textIntro.Size = new System.Drawing.Size(173, 22);
+            this.textIntro.TabIndex = 5;
+            // 
+            // lblinfo
+            // 
+            this.lblinfo.AutoSize = true;
+            this.lblinfo.Location = new System.Drawing.Point(16, 263);
+            this.lblinfo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblinfo.Name = "lblinfo";
+            this.lblinfo.Size = new System.Drawing.Size(74, 17);
+            this.lblinfo.TabIndex = 9;
+            this.lblinfo.Text = "Insert Text";
+            // 
+            // lblinfoLeft
+            // 
+            this.lblinfoLeft.AutoSize = true;
+            this.lblinfoLeft.Location = new System.Drawing.Point(16, 11);
+            this.lblinfoLeft.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblinfoLeft.Name = "lblinfoLeft";
+            this.lblinfoLeft.Size = new System.Drawing.Size(104, 17);
+            this.lblinfoLeft.TabIndex = 7;
+            this.lblinfoLeft.Text = "Nº of elements:";
+            // 
+            // lblInfoRight
+            // 
+            this.lblInfoRight.AutoSize = true;
+            this.lblInfoRight.Location = new System.Drawing.Point(143, 11);
+            this.lblInfoRight.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblInfoRight.Name = "lblInfoRight";
+            this.lblInfoRight.Size = new System.Drawing.Size(104, 17);
+            this.lblInfoRight.TabIndex = 8;
+            this.lblInfoRight.Text = "Selected index:";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 200;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnAdd;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(757, 554);
+            this.Controls.Add(this.lblInfoRight);
+            this.Controls.Add(this.lblinfoLeft);
+            this.Controls.Add(this.lblinfo);
+            this.Controls.Add(this.textIntro);
+            this.Controls.Add(this.btnMoveLeft);
+            this.Controls.Add(this.btnMoveRight);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Bol4_ej5";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnMoveRight;
+        private System.Windows.Forms.Button btnMoveLeft;
+        private System.Windows.Forms.TextBox textIntro;
+        private System.Windows.Forms.Label lblinfo;
+        private System.Windows.Forms.Label lblinfoLeft;
+        private System.Windows.Forms.Label lblInfoRight;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Bol4_ej5/Bol4_ej5/Bol4_ej5/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+
+namespace Bol4_ej5
+{
+    public partial class Form1 : Form
+    {
+        ToolTip tooltip;
+        public delegate void Delegate_ej5();
+        public Delegate_ej5 buttonsDelegate;
+        public Hashtable hash;
+
+        string titulo;
+        string titleChanged = "";
+        char chara;
+        string directorio = Directory.GetCurrentDirectory();
+        double ms = 0;
+        int i;
+        bool iconChange = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            tooltip = new ToolTip();
+            tooltip.SetToolTip(btnAdd, "Adds a new element to ListBox1");
+            tooltip.SetToolTip(btnRemove, "Removes the selected elements from ListBox1");
+            tooltip.SetToolTip(btnMoveRight, "Moves the selected elements from ListBox1 to ListBox2");
+            tooltip.SetToolTip(btnMoveLeft, "Moves one selected element from ListBox2 to ListBox1");
+            tooltip.SetToolTip(listBox2, "Number of elements inside: " + listBox2.Items.Count);
+            textIntro.Select(); //Tiene el foco al iniciar el programa
+            titulo = this.Text;
+            hash = new Hashtable();
+            hash.Add(btnAdd.Text, (Delegate_ej5)(() => lblinfo.Text = "Funciono Add"));
+        }
+
+        public void ButtonsClick(object sender, EventArgs e) // No va
+        {
+            buttonsDelegate = (Delegate_ej5)hash[((Button)sender).Text];
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(textIntro.Text))
+            {
+                listBox1.Items.Add(textIntro.Text);
+                textIntro.Text = "";
+            }
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e) //Recorre los elementos seleccionados y los va borrando de último a primero
+        {
+            if (listBox1.Items.Count > 0)
+            {
+                for (int i = listBox1.SelectedItems.Count - 1; i >= 0; i--)
+                {
+                    listBox1.Items.Remove(listBox1.SelectedItems[i]);
+                }
+            }
+        }
+
+        private void btnMoveRight_Click(object sender, EventArgs e) // Se pasan VARIOS elementos
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                for (int i = 0; i < listBox1.SelectedItems.Count; i++)
+                {
+                    listBox2.Items.Insert(i,listBox1.SelectedItems[i]);
+                }
+
+                btnRemove_Click(sender, e); // Después de pasar elimina los elementos
+            }
+
+            tooltip.SetToolTip(listBox2, "Number of elements inside: " + listBox2.Items.Count);
+        }
+
+        private void btnMoveLeft_Click(object sender, EventArgs e) // Se pasa solo UN elemento
+        {
+            if (listBox2.SelectedItem != null)
+            {
+                listBox1.Items.Insert(0, listBox2.SelectedItem);
+                listBox2.Items.Remove(listBox2.SelectedItem);
+            }
+
+            tooltip.SetToolTip(listBox2, "Number of elements inside: " + listBox2.Items.Count);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Exit program?", "Bol4_ej5",
+                MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.OK)
+            {
+
+            }
+            else
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private string listIndexes()
+        {
+            string indices = "";
+            for (int i = 0; i < listBox1.SelectedItems.Count; i++)
+            {
+                indices = indices + ", " + i.ToString();
+            }
+
+            return indices;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lblinfoLeft.Text = "Nº elementos: " + listBox1.Items.Count.ToString();
+            lblInfoRight.Text = listIndexes();
+
+            if (this.Text.Equals("Bol4_ej5"))
+            {
+                i = titulo.Length - 1;
+                titleChanged = "";
+            }
+
+            chara = titulo[i];
+            titleChanged = chara + titleChanged;
+            this.Text = titleChanged;
+            i--;
+
+            if (ms == 100)
+            {
+                ms = 0;
+            }
+
+            ms++;
+
+            if ((ms % 2) == 0 && iconChange == true)
+            {
+                this.Icon = new Icon(directorio + "\\bocatagarto.ico");
+                iconChange = false;
+            }
+            else
+            {
+                this.Icon = new Icon(directorio + "\\burgallina.ico");
+                iconChange = true;
+            }
+        }
+    }
+}

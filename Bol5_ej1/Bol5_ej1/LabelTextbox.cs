cb88c3b8f2ebe66471d0c8474dd781bd197a9e13
Álvaro <51362780+SkulleD@users.noreply.github.com>
2022-01-13 12:40:58 +0100

    Add files via upload

---
+++ Bol5_ej1/Bol5_ej1/LabelTextbox.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bol5_ej1
+{
+    public enum ePosicion
+    {
+        IZQUIERDA, DERECHA
+    }
+
+    public partial class LabelTextbox : UserControl
+    {
+        public LabelTextbox()
+        {
+            InitializeComponent();
+            Text_Label = Name;
+            Text_TextBox = "";
+            recolocar();
+        }
+
+
+        void recolocar()
+        {
+            switch (posicion)
+            {
+                case ePosicion.IZQUIERDA:
+                    lbl.Location = new Point(0, 0);
+                    txt.Location = new Point(lbl.Width + Separacion, 0); // Label + TextBox
+                    txt.Width = this.Width - lbl.Width - Separacion;
+                    this.Height = Math.Max(txt.Height, lbl.Height);
+                    break;
+                case ePosicion.DERECHA:
+                    txt.Location = new Point(0, 0);
+                    lbl.Location = new Point(txt.Width + Separacion, 0); // TextBox + Label
+                    txt.Width = this.Width - lbl.Width - Separacion;
+                    this.Height = Math.Max(txt.Height, lbl.Height);
+                    break;
+            }
+        }
+
+        private ePosicion posicion = ePosicion.IZQUIERDA;
+
+        [Category("Appearance")]
+        [Description("Indica si la Label se sitúa a la IZQUIERDA o DERECHA del TextBox")]
+        public ePosicion Posicion
+        {
+            set
+            {
+                if (Enum.IsDefined(typeof(ePosicion), value))
+                {
+                    posicion = value;
+                    recolocar();
+                } else
+                {
+                    throw new InvalidEnumArgumentException();
+                }
+            }
+
+            get
+            {
+                return posicion;
+            }
+        }
+
+        private int separacion = 0;
+
+        [Category("Design")]
+        [Description("Píxeles de separación entre Label y TextBox")]
+        public int Separacion
+        {
+            set
+            {
+                if (value >= 0)
+                {
+                    separacion = value;
+                    recolocar();
+                }
+            }
+
+            get
+            {
+                return separacion;
+            }
+        }
+
+        [Category("Appearance")]
+        [Description("Texto asociado a la Label del control")]
+        public string Text_Label
+        {
+            set
+            {
+                lbl.Text = value;
+                recolocar();
+            }
+
+            get
+            {
+                return lbl.Text;
+            }
+        }
+
+        [Category("Appearance")]
+        [Description("Texto asociado al TextBox del control")]
+        public string Text_TextBox
+        {
+            set
+            {
+                txt.Text = value;
+            }
+
+            get
+            {
+                return txt.Text;
+            }
+        }
+
+        protected override void OnSizeChanged(EventArgs e) // Se acomoda al tamaño que se le ponga en formulario
+        {
+            base.OnSizeChanged(e);
+            recolocar();
+        }
+
+        private void txt_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            this.OnKeyPress(e);
+        }
+    }
+}
---
+++ Bol5_ej1/Bol5_ej1Form/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿
+namespace Bol5_ej1Form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelTextbox1 = new Bol5_ej1.LabelTextbox();
+            this.SuspendLayout();
+            // 
+            // labelTextbox1
+            // 
+            this.labelTextbox1.Location = new System.Drawing.Point(31, 29);
+            this.labelTextbox1.Name = "labelTextbox1";
+            this.labelTextbox1.Posicion = Bol5_ej1.ePosicion.IZQUIERDA;
+            this.labelTextbox1.Separacion = 30;
+            this.labelTextbox1.Size = new System.Drawing.Size(156, 20);
+            this.labelTextbox1.TabIndex = 0;
+            this.labelTextbox1.Text_Label = "label1";
+            this.labelTextbox1.Text_TextBox = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelTextbox1);
+            this.Name = "Form1";
+            this.Text = "Bol5_ej1";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Bol5_ej1.LabelTextbox labelTextbox1;
+    }
+}
+

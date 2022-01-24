24a462f7adb956f7684a79db6e50308a0fe75985
Álvaro <51362780+SkulleD@users.noreply.github.com>
2022-01-24 13:50:41 +0100

    Add files via upload

---
+++ Bol5_ej2/Bol5_ej2/EtiquetaAviso.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bol5_ej2
+{
+    public enum eMarca
+    {
+        Nada,
+        Cruz,
+        Circulo
+    }
+
+    public partial class EtiquetaAviso : UserControl
+    {
+        public EtiquetaAviso()
+        {
+            InitializeComponent();
+        }
+
+        private eMarca marca = eMarca.Nada;
+
+        [Category("Appearance")]
+        [Description("Indica el tipo de marca que aparece junto al texto")]
+        public eMarca Marca
+        {
+            set
+            {
+                marca = value;
+                this.Refresh();
+            }
+
+            get
+            {
+                return marca;
+            }
+        }
+
+        protected override void OnTextChanged(EventArgs e)
+        {
+            base.OnTextChanged(e);
+            this.Refresh();
+        }
+
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            base.OnPaint(pe);
+            Graphics graphics = pe.Graphics;
+            int grosor = 0;
+            int offsetX = 0;
+            int offsetY = 0;
+            int altura = this.Font.Height;
+
+            graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+            switch (Marca)
+            {
+                case eMarca.Cruz:
+                    grosor = 3;
+                    Pen lapiz = new Pen(Color.Red, grosor);
+                    graphics.DrawLine(lapiz, grosor, grosor, altura, altura);
+                    graphics.DrawLine(lapiz, altura, grosor, grosor, altura);
+                    offsetX = altura + grosor;
+                    offsetY = grosor / 2;
+                    lapiz.Dispose();
+                    break;
+                case eMarca.Circulo:
+                    grosor = 20;
+                    graphics.DrawEllipse(new Pen(Color.Green, grosor), grosor, grosor, altura, altura);
+                    offsetX = altura + grosor;
+                    offsetY = grosor;
+                    break;
+            }
+
+            SolidBrush brush = new SolidBrush(this.ForeColor);
+            graphics.DrawString(this.Text, this.Font, brush, offsetX + grosor, offsetY);
+            Size size = graphics.MeasureString(this.Text, this.Font).ToSize();
+            this.Size = new Size(size.Width + offsetX, size.Height + offsetY * 2);
+            this.Text = "sfdjdsfjdgd";
+            brush.Dispose();
+        }
+    }
+}
---
+++ Bol5_ej2/Bol5_ej2Form/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace Bol5_ej2Form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.etiquetaAviso1 = new Bol5_ej2.EtiquetaAviso();
+            this.SuspendLayout();
+            // 
+            // etiquetaAviso1
+            // 
+            this.etiquetaAviso1.Location = new System.Drawing.Point(15, 2);
+            this.etiquetaAviso1.Marca = Bol5_ej2.eMarca.Circulo;
+            this.etiquetaAviso1.Name = "etiquetaAviso1";
+            this.etiquetaAviso1.Size = new System.Drawing.Size(90, 53);
+            this.etiquetaAviso1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.etiquetaAviso1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Bol5_ej2";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Bol5_ej2.EtiquetaAviso etiquetaAviso1;
+    }
+}
+

6bdc8971979efe4701433f393642c398930aa0b8
Álvaro <51362780+SkulleD@users.noreply.github.com>
2022-02-24 18:57:21 +0100

    Add files via upload

---
+++ Bol5_ej3/Reproductor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bol5_ej3
+{
+    public partial class Reproductor : UserControl
+    {
+        public Reproductor()
+        {
+            InitializeComponent();
+
+        }
+    }
+}

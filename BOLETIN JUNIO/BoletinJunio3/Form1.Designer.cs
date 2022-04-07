29780515cdcd7bee02152a32288a1a5a9d1a8d7c
Álvaro <51362780+SkulleD@users.noreply.github.com>
2022-04-07 20:22:31 +0200

    Add files via upload

---
+++ BOLETIN JUNIO/BoletinJunio3/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace BoletinJunio3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtRed = new System.Windows.Forms.TextBox();
+            this.txtGreen = new System.Windows.Forms.TextBox();
+            this.txtBlue = new System.Windows.Forms.TextBox();
+            this.labelR = new System.Windows.Forms.Label();
+            this.labelG = new System.Windows.Forms.Label();
+            this.labelB = new System.Windows.Forms.Label();
+            this.btnColor = new System.Windows.Forms.Button();
+            this.btnImage = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.labelBG = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtRed
+            // 
+            this.txtRed.Location = new System.Drawing.Point(12, 56);
+            this.txtRed.Name = "txtRed";
+            this.txtRed.Size = new System.Drawing.Size(47, 22);
+            this.txtRed.TabIndex = 0;
+            // 
+            // txtGreen
+            // 
+            this.txtGreen.Location = new System.Drawing.Point(65, 56);
+            this.txtGreen.Name = "txtGreen";
+            this.txtGreen.Size = new System.Drawing.Size(47, 22);
+            this.txtGreen.TabIndex = 1;
+            // 
+            // txtBlue
+            // 
+            this.txtBlue.Location = new System.Drawing.Point(118, 56);
+            this.txtBlue.Name = "txtBlue";
+            this.txtBlue.Size = new System.Drawing.Size(47, 22);
+            this.txtBlue.TabIndex = 2;
+            // 
+            // labelR
+            // 
+            this.labelR.AutoSize = true;
+            this.labelR.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelR.ForeColor = System.Drawing.Color.Red;
+            this.labelR.Location = new System.Drawing.Point(12, 36);
+            this.labelR.Name = "labelR";
+            this.labelR.Size = new System.Drawing.Size(19, 17);
+            this.labelR.TabIndex = 3;
+            this.labelR.Text = "R";
+            // 
+            // labelG
+            // 
+            this.labelG.AutoSize = true;
+            this.labelG.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelG.ForeColor = System.Drawing.Color.Lime;
+            this.labelG.Location = new System.Drawing.Point(62, 36);
+            this.labelG.Name = "labelG";
+            this.labelG.Size = new System.Drawing.Size(20, 17);
+            this.labelG.TabIndex = 4;
+            this.labelG.Text = "G";
+            // 
+            // labelB
+            // 
+            this.labelB.AutoSize = true;
+            this.labelB.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelB.ForeColor = System.Drawing.Color.Blue;
+            this.labelB.Location = new System.Drawing.Point(115, 36);
+            this.labelB.Name = "labelB";
+            this.labelB.Size = new System.Drawing.Size(18, 17);
+            this.labelB.TabIndex = 5;
+            this.labelB.Text = "B";
+            // 
+            // btnColor
+            // 
+            this.btnColor.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnColor.Location = new System.Drawing.Point(200, 49);
+            this.btnColor.Name = "btnColor";
+            this.btnColor.Size = new System.Drawing.Size(75, 37);
+            this.btnColor.TabIndex = 6;
+            this.btnColor.Text = "COLOR";
+            this.btnColor.UseVisualStyleBackColor = true;
+            this.btnColor.Click += new System.EventHandler(this.btnColor_Click);
+            // 
+            // btnImage
+            // 
+            this.btnImage.Location = new System.Drawing.Point(296, 49);
+            this.btnImage.Name = "btnImage";
+            this.btnImage.Size = new System.Drawing.Size(75, 37);
+            this.btnImage.TabIndex = 7;
+            this.btnImage.Text = "IMAGE";
+            this.btnImage.UseVisualStyleBackColor = true;
+            this.btnImage.Click += new System.EventHandler(this.btnImage_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 128);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(356, 22);
+            this.textBox1.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 108);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 17);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Image Path";
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(538, 12);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(79, 41);
+            this.btnExit.TabIndex = 10;
+            this.btnExit.Text = "EXIT";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // labelBG
+            // 
+            this.labelBG.AutoSize = true;
+            this.labelBG.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelBG.Location = new System.Drawing.Point(238, 24);
+            this.labelBG.Name = "labelBG";
+            this.labelBG.Size = new System.Drawing.Size(94, 17);
+            this.labelBG.TabIndex = 11;
+            this.labelBG.Text = "Background";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnColor;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(629, 450);
+            this.Controls.Add(this.labelBG);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnImage);
+            this.Controls.Add(this.btnColor);
+            this.Controls.Add(this.labelB);
+            this.Controls.Add(this.labelG);
+            this.Controls.Add(this.labelR);
+            this.Controls.Add(this.txtBlue);
+            this.Controls.Add(this.txtGreen);
+            this.Controls.Add(this.txtRed);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Colores e Imágenes";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtRed;
+        private System.Windows.Forms.TextBox txtGreen;
+        private System.Windows.Forms.TextBox txtBlue;
+        private System.Windows.Forms.Label labelR;
+        private System.Windows.Forms.Label labelG;
+        private System.Windows.Forms.Label labelB;
+        private System.Windows.Forms.Button btnColor;
+        private System.Windows.Forms.Button btnImage;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label labelBG;
+    }
+}
+
---
+++ BOLETIN JUNIO/BoletinJunio3/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BoletinJunio3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnColor_Click(object sender, EventArgs e)
+        {
+            BackgroundColor();
+        }
+
+        private void btnImage_Click(object sender, EventArgs e)
+        {
+            BackgroundImage();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void BackgroundColor()
+        {
+
+        }
+
+        private void BackgroundImage()
+        {
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Exit?", "Colores e Imágenes",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}

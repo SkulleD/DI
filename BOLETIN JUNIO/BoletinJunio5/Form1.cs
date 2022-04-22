f7497289a7be053818cb7d47b1ba4e98d034775b
Álvaro <51362780+SkulleD@users.noreply.github.com>
2022-04-22 20:58:42 +0200

    Add files via upload

--- BOLETIN JUNIO/BoletinJunio5/Form1.cs
+++ BOLETIN JUNIO/BoletinJunio5/Form1.cs
@@ -12,34 +12,83 @@ namespace BoletinJunio5
 {
     public partial class Form1 : Form
     {
+        ToolTip tooltip;
+
         public Form1()
         {
             InitializeComponent();
+
+            tooltip = new ToolTip();
+            tooltip.SetToolTip(btnAdd, "Añade un elemento a la Lista 1");
+            tooltip.SetToolTip(btnRemove, "Elimina los elementos seleccionados de la Lista 1");
+            tooltip.SetToolTip(btnToRight, "Mueve los elementos seleccionados de la Lista 1 a la Lista 2");
+            tooltip.SetToolTip(btnToLeft, "Mueve el elemento seleccionado de la Lista 2 a la lista 1");
+            tooltip.SetToolTip(listBox2, $"Nº de elementos actualmente: {listBox2.Items.Count}");
+            textBox1.Select();
         }
 
-        private void btnAdd_Click(object sender, EventArgs e)
+        private void btnAdd_Click(object sender, EventArgs e) // AÑADIR
         {
+            if (!string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                listBox1.Items.Add(textBox1.Text);
+                textBox1.Text = "";
+            }
 
+            textBox1.Select();
         }
 
-        private void btnRemove_Click(object sender, EventArgs e)
+        private void btnRemove_Click(object sender, EventArgs e) // ELIMINAR
         {
+            if (listBox1.SelectedItems.Count > 0) // También parece valer listBox1.Items.Count
+            {
+                for (int i = listBox1.SelectedItems.Count - 1; i >= 0; i--) // Parece que SelectedIndices también funciona??
+                {
+                    listBox1.Items.Remove(listBox1.SelectedItems[i]);
+                }
+            }
+        }   
 
-        }
-
-        private void btnToRight_Click(object sender, EventArgs e)
+        private void btnToRight_Click(object sender, EventArgs e) // PASAR DE LISTA 1 A LISTA 2
         {
+            if (listBox1.SelectedItems != null)
+            {
+                for (int i = 0; i < listBox1.SelectedItems.Count; i++)
+                {
+                    listBox2.Items.Insert(i, listBox1.SelectedItems[i]);
+                }
 
+                for (int i = listBox1.SelectedItems.Count - 1; i >= 0; i--) // Después de ser movidos a lista 2 los borra de lista 1
+                {
+                    listBox1.Items.Remove(listBox1.SelectedItems[i]);
+                }
+            }
+
+            tooltip.SetToolTip(listBox2, $"Nº de elementos actualmente: {listBox2.Items.Count}");
         }
 
         private void btnToLeft_Click(object sender, EventArgs e)
         {
+            if (listBox2.SelectedItem != null)
+            {
+                listBox1.Items.Insert(0, listBox2.SelectedItem);
+                listBox2.Items.Remove(listBox2.SelectedItem);
+            }
 
+            tooltip.SetToolTip(listBox2, $"Nº de elementos actualmente: {listBox2.Items.Count}");
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
 
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("¿Salir?", "Boletín Junio ej 5", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
         }
     }
 }

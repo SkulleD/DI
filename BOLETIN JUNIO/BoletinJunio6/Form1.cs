8db60beea4be1947b76c3a14ce7fbc31e9c16d6f
Álvaro <51362780+SkulleD@users.noreply.github.com>
2022-04-26 17:15:47 +0200

    Add files via upload

--- BOLETIN JUNIO/BoletinJunio6/Form1.cs
+++ BOLETIN JUNIO/BoletinJunio6/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,7 +15,7 @@ namespace BoletinJunio6
     {
         Form2 form2 = new Form2();
         int left = 0;
-        int top = 40;
+        int top = 60;
         Button btn;
 
         public Form1()
@@ -34,7 +35,7 @@ namespace BoletinJunio6
                     if (left == 240)
                     {
                         left = 0;
-                        top += 40;
+                        top += 30;
                     }
 
                     btn = new Button();
@@ -42,6 +43,9 @@ namespace BoletinJunio6
                     btn.Top = top;
                     btn.Text = $"{i}";
                     btn.Name = "btn" + i;
+                    btn.Click += new EventHandler(buttonClick);
+                    btn.MouseEnter += new EventHandler(buttonEnter);
+                    btn.MouseLeave += new EventHandler(buttonLeave);
                     Controls.Add(btn);
 
                     left += 80;
@@ -49,9 +53,12 @@ namespace BoletinJunio6
                 else
                 {
                     btn = new Button();
-                    btn.Top = 160;
+                    btn.Top = 150;
                     btn.Text = "*";
                     btn.Name = "btnAsterisco";
+                    btn.Click += new EventHandler(buttonClick);
+                    btn.MouseEnter += new EventHandler(buttonEnter);
+                    btn.MouseLeave += new EventHandler(buttonLeave);
                     Controls.Add(btn);
                 }
             }
@@ -62,21 +69,90 @@ namespace BoletinJunio6
                 {
                     btn = new Button();
                     btn.Left = 80;
-                    btn.Top = 160;
+                    btn.Top = 150;
                     btn.Text = $"{i}";
                     btn.Name = "btn" + i;
+                    btn.Click += new EventHandler(buttonClick);
+                    btn.MouseEnter += new EventHandler(buttonEnter);
+                    btn.MouseLeave += new EventHandler(buttonLeave);
                     Controls.Add(btn);
                 }
                 else
                 {
                     btn = new Button();
                     btn.Left = 160;
-                    btn.Top = 160;
+                    btn.Top = 150;
                     btn.Text = "#";
                     btn.Name = "btnAlmohadilla";
+                    btn.Click += new EventHandler(buttonClick);
+                    btn.MouseEnter += new EventHandler(buttonEnter);
+                    btn.MouseLeave += new EventHandler(buttonLeave);
                     Controls.Add(btn);
                 }
             }
+        }
+
+        private void buttonClick(object sender, EventArgs e)
+        {
+            Button boton = (Button)sender;
+
+            if (boton.Text.ToLower().Equals("reset"))
+            {
+                textBox1.Text = "";
+            }
+            else
+            {
+                textBox1.Text += boton.Text;
+                boton.BackColor = Color.Yellow;
+            }
+        }
+
+        private void buttonEnter(object sender, EventArgs e)
+        {
+            Button boton = (Button)sender;
+
+            if (!(boton.BackColor == Color.Yellow))
+            {
+                boton.BackColor = Color.LightGreen;
+            }
+        }
+
+        private void buttonLeave(object sender, EventArgs e)
+        {
+            Button boton = (Button)sender;
+
+            if (!(boton.BackColor == Color.Yellow))
+            {
+                boton.BackColor = Color.Transparent;
+            }
+        }
+
+        private void grabarNúmeroToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            StreamWriter writer;
+            SaveFileDialog fileDialog = new SaveFileDialog();
+            fileDialog.Filter = "Text File | *.txt";
+            int numero;
+            bool esNum = false;
+            string path = Directory.GetCurrentDirectory();
+
+            esNum = int.TryParse(textBox1.Text, out numero);
+
+            if (!string.IsNullOrWhiteSpace(textBox1.Text) && esNum)
+            {
+                if (fileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    using (writer = new StreamWriter(path))
+                    {
+                        writer.Write(textBox1.Text);
+                    }
+                }
+            }
+        }
+
+        private void resetToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
         }
 
         private void salirToolStripMenuItem_Click(object sender, EventArgs e)

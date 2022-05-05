2d4d688c0602bed2837e3db410d428802e1a14d4
Álvaro <51362780+SkulleD@users.noreply.github.com>
2022-05-05 12:22:38 +0200

    Add files via upload

---
+++ BOLETIN JUNIO/BoletinJunio8/BoletinJunio8/EtiquetaAviso.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BoletinJunio8
+{
+    public enum eMarca
+    {
+        Nada,
+        Cruz,
+        Circulo
+    }
+
+    public partial class EtiquetaAviso : Control
+    {
+        public EtiquetaAviso()
+        {
+            InitializeComponent();
+        }
+
+        private eMarca marca = eMarca.Nada;
+
+        [Category("Appearance")]
+        [Description("Indica la marca que aparecerá junto al texto")]
+        public eMarca Marca
+        {
+            set
+            {
+                marca = value;
+                Refresh();
+            }
+
+            get
+            {
+                return marca;
+            }
+        }
+    }
+}

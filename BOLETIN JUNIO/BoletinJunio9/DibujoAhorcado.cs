d3e1890aee2d788230ef0caf83ad8f45f460dc52
Álvaro <51362780+SkulleD@users.noreply.github.com>
2022-05-09 15:33:35 +0200

    Add files via upload

---
+++ BOLETIN JUNIO/BoletinJunio9/DibujoAhorcado.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BoletinJunio9
+{
+    public partial class DibujoAhorcado : Control
+    {
+        public DibujoAhorcado()
+        {
+            InitializeComponent();
+        }
+
+        private int errores = 0;
+
+        [Category("Ahorcado")]
+        [Description("Número de errores al no acertar una letra")]
+        public int Errores
+        {
+            set
+            {
+                errores = value;
+            }
+
+            get
+            {
+                return errores;
+            }
+        }
+    }
+}

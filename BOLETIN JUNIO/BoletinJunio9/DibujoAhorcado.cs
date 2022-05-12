88294918e5d346e5e55641774e034d5201a5b9c0
Álvaro <51362780+SkulleD@users.noreply.github.com>
2022-05-12 22:50:12 +0200

    Add files via upload

--- BOLETIN JUNIO/BoletinJunio9/DibujoAhorcado.cs
+++ BOLETIN JUNIO/BoletinJunio9/DibujoAhorcado.cs
@@ -12,6 +12,8 @@ namespace BoletinJunio9
 {
     public partial class DibujoAhorcado : Control
     {
+        private bool addLinea = false;
+
         public DibujoAhorcado()
         {
             InitializeComponent();
@@ -20,13 +22,14 @@ namespace BoletinJunio9
         private int errores = 0;
 
         [Category("Ahorcado")]
-        [Description("Número de errores al no acertar una letra")]
+        [Description("Número de errores al no acertar una letra.")]
         public int Errores
         {
             set
             {
                 if (value != errores)
                 {
+                    addLinea = true;
                     CambiaError?.Invoke(this, new EventArgs());
                 }
 
@@ -40,11 +43,11 @@ namespace BoletinJunio9
         }
 
         [Category("Ahorcado")]
-        [Description("Se lanza cada vez que cambia el número de errores")]
+        [Description("Se lanza cada vez que cambia el número de errores.")]
         public event EventHandler CambiaError;
 
         [Category("Ahorcado")]
-        [Description("Se lanza cuando el dibujo del ahorcado es completado")]
+        [Description("Se lanza cuando el dibujo del ahorcado es completado.")]
         public event EventHandler Ahorcado;
 
         protected override void OnPaint(PaintEventArgs e)
@@ -55,24 +58,49 @@ namespace BoletinJunio9
             graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
             Pen pen = new Pen(Color.Black);
 
-            int centrar = Right - Left;
             // Point brazos = new Point(Width / 2, 40);
             // Point piernas = new Point(Width / 2, 80);
 
-            //Horca
-            graphics.DrawLine(pen, 0, Height - 1, centrar, Height - 1); // Base
-            graphics.DrawLine(pen, 0, Height, 0, 15); // Soporte
-            graphics.DrawLine(pen, 0, 15, Width / 2, 15); // Techo
-            graphics.DrawLine(pen, Width / 2, 15, Width / 2, 25); // Soga
+            graphics.DrawLine(pen, 0, Height - 1, Right - Left, Height - 1); // Base
 
-            //Ahorcado
-            graphics.DrawEllipse(pen, (Width / 2) - 8, 25, 15, 15); // Cabeza
-            graphics.DrawLine(pen, Width / 2, 40, Width / 2, 80); // Tronco
-            graphics.DrawLine(pen, Width / 2, 40, (Width / 2) - 10, 75); // Brazo 1
-            graphics.DrawLine(pen, Width / 2, 40, (Width / 2) + 10, 75); // Brazo 2
-            graphics.DrawLine(pen, Width / 2, 80, (Width / 2) - 10, 120); // Pierna 1
-            graphics.DrawLine(pen, Width / 2, 80, (Width / 2) + 10, 120); // Pierna 2
+            if (addLinea)
+            {
+                switch (errores)
+                {
+                    //Horca
+                    case 1:
+                        graphics.DrawLine(pen, 0, Height, 0, 15); // Soporte
+                        break;
+                    case 2:
+                        graphics.DrawLine(pen, 0, 15, Width / 2, 15); // Techo
+                        break;
+                    case 3:
+                        graphics.DrawLine(pen, Width / 2, 15, Width / 2, 25); // Soga
+                        break;
 
+                    //Ahorcado
+                    case 4:
+                        graphics.DrawEllipse(pen, (Width / 2) - 8, 25, 15, 15); // Cabeza
+                        break;
+                    case 5:
+                        graphics.DrawLine(pen, Width / 2, 40, Width / 2, 80); // Tronco
+                        break;
+                    case 6:
+                        graphics.DrawLine(pen, Width / 2, 40, (Width / 2) - 10, 75); // Brazo 1
+                        break;
+                    case 7:
+                        graphics.DrawLine(pen, Width / 2, 40, (Width / 2) + 10, 75); // Brazo 2
+                        break;
+                    case 8:
+                        graphics.DrawLine(pen, Width / 2, 80, (Width / 2) - 10, 120); // Pierna 1
+                        break;
+                    case 9:
+                        graphics.DrawLine(pen, Width / 2, 80, (Width / 2) + 10, 120); // Pierna 2
+                        break;
+                }
+            }
+
+            addLinea = false;
             pen.Dispose();
         }
     }
--- BOLETIN JUNIO/BoletinJunio9/FormBol9/Form1.cs
+++ BOLETIN JUNIO/BoletinJunio9/FormBol9/Form1.cs
@@ -12,9 +12,60 @@ namespace FormBol9
 {
     public partial class Form1 : Form
     {
+        private string palabra;
+
         public Form1()
         {
             InitializeComponent();
+            palabra = "CACAOLAT";
+            lblUsadas.Text += "\r\n";
+            DibujaPalabra("");
+        }
+
+        private void btnUsarLetra_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtLetra.Text) && txtLetra.Text.Length == 1)
+            {
+                if (palabra.Contains(txtLetra.Text.ToUpper()))
+                {
+                    DibujaPalabra(txtLetra.Text.ToUpper());
+                }
+                else
+                {
+                    dibujoAhorcado1.Errores++;
+                    lblUsadas.Text += $"{txtLetra.Text.ToUpper()}, ";
+                }
+
+                txtLetra.Text = "";
+                txtLetra.Select();
+            }
+        }
+
+        private void dibujoAhorcado1_CambiaError(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dibujoAhorcado1_Ahorcado(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DibujaPalabra(string letra)
+        {
+            lblPalabra.Text = "";
+
+            for (int i = 0; i <= palabra.Length; i++)
+            {
+                if (palabra[0].Equals(letra))
+                {
+                    lblPalabra.Text += $"{letra.ToUpper()} ";
+                }
+                else
+                {
+                    lblPalabra.Text += "_ ";
+                }
+            }
         }
     }
 }

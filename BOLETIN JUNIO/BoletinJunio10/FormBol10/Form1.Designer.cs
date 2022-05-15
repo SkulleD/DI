78c3c8c3e6efcf4578ee4db4ad8be569731ba58d
Álvaro <51362780+SkulleD@users.noreply.github.com>
2022-05-15 21:23:11 +0200

    Add files via upload

---
+++ BOLETIN JUNIO/BoletinJunio10/FormBol10/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace FormBol10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.validateTextBox1 = new BoletinJunio10.ValidateTextBox();
+            this.SuspendLayout();
+            // 
+            // validateTextBox1
+            // 
+            this.validateTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.validateTextBox1.Name = "validateTextBox1";
+            this.validateTextBox1.Size = new System.Drawing.Size(150, 42);
+            this.validateTextBox1.TabIndex = 0;
+            this.validateTextBox1.Texto = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(531, 438);
+            this.Controls.Add(this.validateTextBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Frikilove";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private BoletinJunio10.ValidateTextBox validateTextBox1;
+    }
+}
+
